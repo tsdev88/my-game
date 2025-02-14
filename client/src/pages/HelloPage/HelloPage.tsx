8fe7961b3f7c8a6ef37be70ed157cafb2b75ae44
Rasulio05 <mrn_0593@mail.ru>
2025-02-14 15:42:26 +0300

    Relize

--- client/src/pages/HelloPage/HelloPage.tsx
+++ client/src/pages/HelloPage/HelloPage.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { useNavigate } from 'react-router';
+import styles from './HelloPage.module.css';
 
-export default function HelloPage() {
+export default function HelloPage(): React.JSX.Element {
+  const navigate = useNavigate();
+
+  const handlerClick = () => {
+    navigate('/');
+  };
   return (
-    <div>
-        Начать игру
+    <div className={styles.container}>
+      <div className={styles.title}>
+        <h1>Начало Игры</h1>
+      </div>
+
+      <div>
+        <button className={styles.button} onClick={handlerClick}>
+          Играть
+        </button>
+      </div>
     </div>
-  )
+  );
 }
