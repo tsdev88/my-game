e79443bb6b5f6015b0a00b10af7da94dd8b75770
tsdev88 <sholatsering@gmail.com>
2025-02-14 15:52:08 +0300

    modal done

--- client/src/app/routes/RouterProvider.tsx
+++ client/src/app/routes/RouterProvider.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import {Route, Routes} from "react-router";
 import MainPage from "../../pages/ui/MainPage.tsx";
+import ModalQuestion from "../../widgets/ui/ModalQuestion/ModalQuestion.tsx";
 
 export default function RouterProvider(): React.JSX.Element {
     return (
         <Routes>
             <Route path="/" element={<MainPage/>}/>
+            <Route path='/modal' element={<ModalQuestion/>}/>
         </Routes>
     );
 }
